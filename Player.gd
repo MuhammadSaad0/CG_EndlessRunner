1786336bc902d4da20d0c2718c88b081fc210589
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2023-10-29 15:12:14 +0500

    Milestone1

--- Player.gd
+++ Player.gd
@@ -1,34 +1,84 @@
 extends CharacterBody3D
 
-const SPEED = 50.0
-const JUMP_VELOCITY = 8.5
-const MOVE_DISTANCE = 500.0  # Define the fixed movement distance.
+var SPEED = 50.0
+var JUMP_VELOCITY = 8.5
+var dead = 0
+var lane = 1
+var score_label
+var coin_label
+var score = 0
+var coins = 0
+var jumpBoostTimer = 0
+var speedBoostTimer = 0
+signal changeTerrainSpeed(val)
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func change_lane(change):
+	lane += change;
+	if lane > 2:
+		lane = 2
+	if lane < 0:
+		lane = 0
+	
+func _ready():
+	score_label = get_parent_node_3d().get_node('UserInterface/Score')
+	coin_label = get_parent_node_3d().get_node("UserInterface/Coins")
+	var area3D = get_node("Area3D")
+	area3D.connect('area_entered', self.on_collision)
+	area3D.connect('body_entered', self.on_collision2)
+	
+func on_collision(body):
+	if body.is_in_group("Coins"):
+		print("Coin")
+		coins += 20
+		coin_label.text = "Rupees: " + str(coins)
+	elif body.is_in_group("JumpBoost"):
+		print("JumpBoost")
+		jumpBoostTimer = 120
+		JUMP_VELOCITY *= 2
+	elif body.is_in_group("SpeedBoost"):
+		print("SpeedBoost")
+		speedBoostTimer = 300
+		emit_signal("changeTerrainSpeed", 1)
+	elif body.is_in_group("Obstacle"):
+		get_tree().change_scene_to_file('res://Menu.tscn')
+		
+func on_collision2(body):
+	if body.is_in_group("Obstacle"):
+		get_tree().change_scene_to_file('res://Menu.tscn')
+
 func _physics_process(delta):
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta * 1.7
 
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	var input_dir = Vector3()  # Initialize the input direction as a zero vector.
-
-	# Check for left and right inputs and set the appropriate x value in the input_dir vector.
-	if Input.is_action_pressed("left"):
-		input_dir.x = -MOVE_DISTANCE  # Move left by a fixed distance.
-	elif Input.is_action_pressed("right"):
-		input_dir.x = MOVE_DISTANCE  # Move right by a fixed distance.
-
-	# Get the direction vector and normalize it.
-	var direction = (transform.basis * input_dir).normalized()
-
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if jumpBoostTimer > 0:
+		jumpBoostTimer-=1
+	if jumpBoostTimer == 0:
+		JUMP_VELOCITY = 8.5
+		
+	if speedBoostTimer > 0:
+		speedBoostTimer-=1
+	if speedBoostTimer == 0:
+		emit_signal("changeTerrainSpeed", 0)
+		
+	self.transform.origin.x = lerp(self.transform.origin.x, (lane * 7.0) -7.0, delta * 15.0)
+	self.transform.origin.x = clamp(self.transform.origin.x, -7, 7)
+	
+	# Check for left and right inputs
+	if Input.is_action_just_pressed("left"):
+		change_lane(-1) # Move left by a fixed distance.
+	elif Input.is_action_just_pressed("right"):
+		change_lane(1)  # Move right by a fixed distance.
+	if dead != 1:
+		if speedBoostTimer > 0:
+			score += 4
+		else:
+			score += 1
+		score_label.text = "Score: " + str(score)
+	
 
 	move_and_slide()
--- TerrainController.gd
+++ TerrainController.gd
@@ -8,51 +8,81 @@ class_name TerrainController
 ## is added to the far end of the conveyor
 
 ## Holds the catalog of loaded terrian block scenes
-var TerrainBlocks: Array = []
+var TerrainBlocks: Array = [preload("res://terrain_blocks/terrain_1.tscn"), preload("res://terrain_blocks/terrain_2.tscn"), preload("res://terrain_blocks/terrain_3.tscn"), preload("res://terrain_blocks/terrain_4.tscn"), preload("res://terrain_blocks/terrain_5.tscn"), preload("res://terrain_blocks/terrain_6.tscn"), preload("res://terrain_blocks/terrain_7.tscn")]
+var Pickups: Array = []
 ## The set of terrian blocks which are currently rendered to viewport
 var terrain_belt: Array[MeshInstance3D] = []
 @export var terrain_velocity: float = 10.0
+var old_terrain_velocity: float = 10.0
 ## The number of blocks to keep rendered to the viewport
-@export var num_terrain_blocks = 4
+@export var num_terrain_blocks = 10
 ## Path to directory holding the terrain block scenes
 @export_dir var terrian_blocks_path = "res://terrain_blocks"
-
+@export_dir var pickups_path = "res://pickups"
 
 func _ready() -> void:
-	_load_terrain_scenes(terrian_blocks_path)
-	var starting_block = TerrainBlocks.pop_back().instantiate()
-	add_child(starting_block)
-	terrain_belt.append(starting_block)
+	_load_pickups(pickups_path)
 	_init_blocks(num_terrain_blocks)
+	get_parent_node_3d().get_node("Player").connect('changeTerrainSpeed', self.changeTerrainSpeed)
 
 func _physics_process(delta: float) -> void:
 	_progress_terrain(delta)
 
+func changeTerrainSpeed(val):
+	if val == 1:
+		old_terrain_velocity = terrain_velocity
+		terrain_velocity *= 1.5
+		if terrain_velocity > terrain_velocity * 2:
+			terrain_velocity = terrain_velocity * 2
+	else:
+		terrain_velocity = old_terrain_velocity
 
 func _init_blocks(number_of_blocks: int) -> void:
-	
-	for block_index in number_of_blocks:
-		var block = TerrainBlocks.pick_random().instantiate()
-		if block_index == 0:
-			block.position.z = block.mesh.size.y/2
-		else:
-			_append_to_far_edge(terrain_belt[block_index-1], block)
+		
+		var block
+		block = TerrainBlocks[0].instantiate()
+		block.position.z = block.mesh.size.y/2
 		add_child(block)
 		terrain_belt.append(block)
+		block = TerrainBlocks[0].instantiate()
+		_append_to_far_edge(terrain_belt[0], block)
+		add_child(block)
+		terrain_belt.append(block)
+		for i in range(number_of_blocks):
+			block = TerrainBlocks.pick_random().instantiate()
+			_append_to_far_edge(terrain_belt[i+1], block)
+			add_child(block)
+			terrain_belt.append(block)
 
 
 func _progress_terrain(delta: float) -> void:
+	
 	for block in terrain_belt:
 		block.position.z += terrain_velocity * delta
 
-	if terrain_belt[0].position.z >= terrain_belt[0].mesh.size.y/2:
+	if terrain_belt[0].position.z >= terrain_belt[0].mesh.size.y/0.5:
 		var last_terrain = terrain_belt[-1]
+		var first_terrain = terrain_belt.pop_front()
 
 		var block = TerrainBlocks.pick_random().instantiate()
+		var posrand = randi()%3
+		var pickupSpawn = randi_range(0, 1)
+		if pickupSpawn == 1:
+			if posrand == 0:
+				posrand = -7
+			elif posrand == 1:
+				posrand = 0
+			elif posrand == 2:
+				posrand = 7
+			var pickup = Pickups.pick_random().instantiate()
+			pickup.transform.origin.x = posrand
+			block.add_child(pickup)
+		
 		_append_to_far_edge(last_terrain, block)
 		add_child(block)
-		terrain_belt.append(block)
 
+		terrain_belt.append(block)
+		first_terrain.queue_free()
 
 
 func _append_to_far_edge(target_block: MeshInstance3D, appending_block: MeshInstance3D) -> void:
@@ -64,3 +94,9 @@ func _load_terrain_scenes(target_path: String) -> void:
 	for scene_path in dir.get_files():
 		print("Loading terrian block scene: " + target_path + "/" + scene_path)
 		TerrainBlocks.append(load(target_path + "/" + scene_path))
+		
+func _load_pickups(target_path: String) -> void:
+	var dir = DirAccess.open(target_path)
+	for scene_path in dir.get_files():
+		print("Loading pickups: " + target_path + "/" + scene_path)
+		Pickups.append(load(target_path + "/" + scene_path))
